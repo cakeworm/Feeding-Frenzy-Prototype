c57a201046bd8f51dd0776a0d2be5e4a33450f2b
cakeworm <rsulkosky1@gmail.com>
2016-08-14 09:36:52 +0900

    Basic FPS code
    basic FPS from the Hocking's Unity in Action

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour 
+{
+
+     public float speed = 3.0f;
+
+
+
+        void Update () 
+
+        {
+    
+            transform.Rotate (0, speed, 0);
+
+        }
+}    
